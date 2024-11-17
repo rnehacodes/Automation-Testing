7d30b6e06a47d2a470563975d0b74454d9c33280
Neha Rajput <neharajputcoding@gmail.com>
2024-11-17 23:44:04 +0530

    Selenium Setup & First Selenium Test

---
+++ seleniumJava/demo/src/main/java/com/selenium/App.java
@@ -0,0 +1,9 @@
+package com.selenium;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is a java selenium automation project");
+    }
+}
