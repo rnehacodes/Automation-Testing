a9d976b7cf5285fe4404a2e41a227f871625cc91
Neha Rajput <neharajputcoding@gmail.com>
2024-11-27 23:59:27 +0530

    Locators #2

--- seleniumAutomation/src/seleniumAutomation/Locators.java
+++ seleniumAutomation/src/seleniumAutomation/Locators.java
@@ -10,24 +10,32 @@ public class Locators {
 		System.setProperty("webdriver.chromedriver", "resources\\drivers\\chromedriver-win64\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
-		
+
 		String url = "https://demo.aeries.net/aeries/Login.aspx";
 		driver.get(url);
-		
-		//Finding Username textbox
+
+
+		//Finding Username textbox using ID
 		WebElement userNameBox = driver.findElement(By.id("Username_Aeries"));
 		userNameBox.sendKeys("admin");
-		
+
+		//Finding Password textbox using ID
 		WebElement passwordBox = driver.findElement(By.id("Password_Aeries"));
 		passwordBox.sendKeys("admin");
-		
+
+		//Finding Sign In button using Name
 		WebElement clickBtn = driver.findElement(By.name("btnSignIn_Aeries"));
 		clickBtn.click();
-		
+
 		Thread.sleep(5000);
-		driver.findElement(By.name("btnContinueIn_Aeries"));
-		//btnContinueIn_Aeries
-		
+
+		//Finding Continue button using Name
+		driver.findElement(By.name("btnContinueIn_Aeries")).;
+
+		//Verifying presence of hyperlink
+		driver.findElement(By.linkText("https://demo.aeries.net/AeriesTX")).click();
+
+
 		driver.quit();
 	}
 
